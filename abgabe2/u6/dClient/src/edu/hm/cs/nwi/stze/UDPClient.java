5dc8f8e2ba617c483464eaadf098629fb59667f0
089 <zell@hm.edu>
2015-11-25 20:56:25 +0100

    Aufgabe 6.1 hinzugefügt

---
+++ abgabe2/u6/dClient/src/edu/hm/cs/nwi/stze/UDPClient.java
@@ -0,0 +1,129 @@
+package edu.hm.cs.nwi.stze;
+
+import com.sun.deploy.util.ArrayUtil;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Sends UDP packets during {@code transmissionTime} to a server.
+ *
+ * Packets consists of a header (8 Byte {@code packetID}, 8 Byte {@code sendTime})
+ * and a body (around 1400 Bytes of data).
+ *
+ * @author: Martin Zell
+ * @version: 0.9
+ */
+
+public class UDPClient {
+
+    public static void main(String[] args) throws UnsupportedEncodingException, UnknownHostException {
+
+        int transmissionTime = 30; // in seconds
+
+        int N = 250; // delay step
+
+        long k = 1000; // delay in millis
+
+        InetAddress serverAddress = InetAddress.getByName("127.0.0.1");
+        //InetAddress serverAddress = InetAddress.getByName("87.106.2.1");
+        //InetAddress serverAddress = InetAddress.getByName("192.168.1.120");
+
+        int serverPort = 4711;
+
+        //#####################################################################
+        // Do not change the following parameters!
+        //#####################################################################
+
+        // String with 1400 Byte of data (UTF-8!)
+        String bodyString = "Eigenschaften des UDP\n" +
+                "UDP ist ein verbindungsloses, nicht-zuverlässiges und ungesichertes wie auch ungeschütztes " +
+                "Übertragungsprotokoll. Das bedeutet, es gibt keine Garantie, dass ein einmal gesendetes Paket auch " +
+                "ankommt, dass Pakete in der gleichen Reihenfolge ankommen, in der sie gesendet wurden, oder dass ein" +
+                " Paket nur einmal beim Empfänger eintrifft. Es gibt auch keine Gewähr dafür, dass die Daten " +
+                "unverfälscht oder unzugänglich für Dritte beim Empfänger eintreffen. Eine Anwendung, die UDP nutzt, " +
+                "muss daher gegenüber verlorengegangenen und unsortierten Paketen unempfindlich sein oder selbst " +
+                "entsprechende Korrekturmaßnahmen und ggfs. auch Sicherungsmaßnahmen vorsehen. Ein Datenschutz ist bei" +
+                " dieser offenen Kommunikation nicht möglich.\n\nDa vor Übertragungsbeginn nicht erst eine Verbindung " +
+                "aufgebaut werden muss, kann ein Partner oder können beide Partner schneller mit dem Datenaustausch" +
+                " beginnen. Das fällt vor allem bei Anwendungen ins Gewicht, bei denen nur kleine Datenmengen " +
+                "ausgetauscht werden müssen. Einfache Frage-Antwort-Protokolle wie DNS (das Domain Name System)" +
+                " verwenden UDP, um die Netzwerkbelastung gering zu halten und damit den Datendurchsatz zu erhöhen. Ein" +
+                " Drei-Wege-Handschlag wie bei TCP (dem Transmission Control Protocol) für den Aufbau der Verbindung [...]" +
+                "\n Quelle: Wikipedia (https://de.wikipedia.org/wiki/User_Datagram_Protocol#Eigenschaften)";
+        //System.out.println(dataString.getBytes("UTF-8").length);
+
+        // convert string to byte
+        byte[] bodyBytes = bodyString.getBytes();
+
+        long startTime = System.currentTimeMillis();
+
+        List<Long> lostPacketsList = new ArrayList<>();
+
+        long packetID = 0;
+
+        byte[] headerID = null;
+
+        byte[] headerSendingTime = null;
+
+        // Transmission
+        while(System.currentTimeMillis() - startTime < transmissionTime * 1000) {
+
+            // Header: ID
+            packetID++;
+            headerID = ByteBuffer.allocate(8).putLong(packetID).array();
+
+            // Heder: sending time
+            headerSendingTime = ByteBuffer.allocate(8).putLong(System.currentTimeMillis()).array();
+
+            // concat header and body data
+            byte[] data = new byte[bodyBytes.length + 2 * 8];
+            ByteBuffer dataBuffer = ByteBuffer.wrap(data);
+            dataBuffer.put(headerID);
+            dataBuffer.put(headerSendingTime);
+            dataBuffer.put(bodyBytes);
+
+            DatagramPacket packet = new DatagramPacket(dataBuffer.array(), dataBuffer.array().length, serverAddress, serverPort );
+
+            try {
+                new DatagramSocket().send(packet);
+            } catch(FileNotFoundException e) {
+                // TODO
+            } catch (IOException e){
+                //e.printStackTrace();
+                System.out.println("" + packetID);
+                lostPacketsList.add(packetID);
+                continue;
+            }
+
+
+        // influence the performance
+            if(packetID % N == 0) {
+                try {
+                    System.out.println("begin sleep");
+                    Thread.sleep(k);
+                    System.out.println("end sleep");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+      System.out.printf("Von %d Paketen wurden nur %d Pakete - mit einer theoretischen Datenrate von %d kbit/s - versendet. %d Pakete konnten nicht gesendet werden."
+              , packetID
+              , (packetID - lostPacketsList.size())
+              , ((((packetID - lostPacketsList.size()) * (bodyBytes.length + 16) * 8)/(transmissionTime))/1024)
+              , lostPacketsList.size()
+              );
+
+    }
+
+}
---
+++ abgabe2/u6/dServer/src/stze/UDPServer.java
@@ -0,0 +1,111 @@
+package stze;
+
+import java.io.IOException;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class UDPServer {
+
+    public static void main(String[] args) {
+
+        int serverPort = 4711;
+        int packetSize = 1416;
+        int timeoutForPerformanceMeasurement = 500; // in millis
+        int maxTimouts = 10; // if there are maxTimeouts then the connection will be closed
+        boolean ignoreMaxTimeouts = false;
+
+        //boolean resetPerformanceTimer = true;
+        long performanceTimer = System.currentTimeMillis();
+        double performance = 0;
+        List<Double> performanceList = new ArrayList<>();
+
+        int countPackets = 0;
+        int countTotalPackets = 0;
+        int countTimeouts = 0;
+
+        try (DatagramSocket socket = new DatagramSocket(serverPort)) {
+
+            socket.setSoTimeout(timeoutForPerformanceMeasurement);
+            System.out.println("UDP server started");
+
+            // System.out.printf("von\t\tID\tTime\tLänge\tRate\n");
+
+            System.out.println("kbit/s;\t\tPakete;\ttotal;\tZeit [s];");
+            System.out.println("---------------------------------");
+            while (true) {
+
+                // Auf Anfrage warten
+                DatagramPacket packet = new DatagramPacket(new byte[packetSize], packetSize);
+
+                try {
+                    socket.receive(packet);
+
+                    countTimeouts = 0;
+                    countPackets++;
+                    countTotalPackets++;
+
+                    //Empfänger auslesen
+                    InetAddress address = packet.getAddress();
+                    int port = packet.getPort();
+                    int len = packet.getLength();
+                    byte[] data = packet.getData();
+
+                    long packetID = ByteBuffer.wrap(data,0,8).getLong();
+                    long sendingTime = ByteBuffer.wrap(data,8,8).getLong();
+
+                    //byte [] subArray = Arrays.copyOfRange(bytes, 4, 6);
+
+                    // System.out.printf("%s:%d\t\t%d\t%d\t%d\t%f\n", address, port, packetID, sendingTime, len, performance);
+
+//                System.out.printf("Anfrage von %s vom Port %d mit der Länge %d:%n%s%n", address, port, len, new String(data, 16, len));
+
+                } catch (SocketTimeoutException e) {
+                    //e.printStackTrace();
+                    // System.out.printf("%d, %d, %d, %d, %d, %d, %d\n", countPackets, packetSize, (countPackets * packetSize * 8), System.currentTimeMillis(), performanceTimer, (System.currentTimeMillis() - performanceTimer), ((System.currentTimeMillis() - performanceTimer)/1000));
+
+                    long bitsOfAllReceivedPackets = countPackets * packetSize * 8;
+                    double timeDiffInSeconds = (System.currentTimeMillis() - performanceTimer)/1000.0;
+
+                    performance = (timeDiffInSeconds > 0) ? (bitsOfAllReceivedPackets/timeDiffInSeconds) / 1024 : 0; // we must avoid by zero division error.
+                    performanceList.add(performance);
+
+                    // Datenrate anzeigen
+                    if(countPackets > 0)
+                        //System.out.printf("Letzte Datenrate: %,.2f kbit/s.\nPakete: %d (total: %d)\nZeit: %2.3f s\n------------------------------------\n"
+                        System.out.printf("%,10.2f;\t%5d;\t%5d;\t%2.3f;\n"
+                                , performance
+                                , countPackets
+                                , countTotalPackets
+                                , timeDiffInSeconds
+                                );
+
+                    performanceTimer = System.currentTimeMillis();
+                    countPackets=0;
+
+                    if(countTimeouts > 0)
+                        System.out.println(maxTimouts-countTimeouts);
+
+                    countTimeouts++;
+                    if(!ignoreMaxTimeouts && countTimeouts > maxTimouts) {
+                        System.out.println("Max timeouts achieved.");
+                        break;
+                    }
+
+                } catch (SocketException e) {
+                    // TODO
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("UDP server stopped");
+        System.out.printf("Es wurden %d Pakete empfangen.", countTotalPackets);
+
+    }
+}
