73908692d65ac9975016f93215da5b8dbe00755f
089 <zell@hm.edu>
2015-12-18 12:28:49 +0100

    Ergänzung in FileSender: Parameter Dateiname und Name Zielrechner

--- abgabe3/ReliableDataTransfer/src/edu/hm/cs/nwi/stze/sender/Main.java
+++ abgabe3/ReliableDataTransfer/src/edu/hm/cs/nwi/stze/sender/Main.java
@@ -9,17 +9,26 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        InetAddress ia = InetAddress.getByName( "localhost" );
+        InetAddress ia;
+        String filename;
         int port = 4711;
+
+        if(args.length == 2) {
+            filename = args[0];
+            ia = InetAddress.getByName(args[1]);
+        } else {
+            filename = "test.txt";
+            ia = InetAddress.getByName( "localhost" );
+        }
 
         System.out.println("Sende Datei ..");
 
         CatSenderSocket socket = new CatSenderSocket(ia, port);
-        try(InputStream in = new FileInputStream("test.txt"))
+        try(InputStream in = new FileInputStream(filename))
         {
             socket.send(in);
         }
 
-        System.out.println("Done!");
+        System.out.println("Fertig!");
     }
 }
